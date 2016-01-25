4079bed5036dfe5984cb721a22a56c3151b7b265
Fede Gratti <federicogratti@gmail.com>
2016-01-25 20:15:03 -0300

    Refactoring to seeds.rb. Now seeds are not depending of the state of the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,24 @@
-Client.create!([{first_name: "Federico", last_name: "Gratti", birthday: "1990-12-13", gender: "M", dni: 34967836, cuit: 20349678368},
-                {first_name: "German", last_name: "Gonzales", birthday: "1994-06-12", gender: "M", dni: 24657867, cuit: 20246578679}])
+client1 = Client.create!({first_name: "Federico", last_name: "Gratti", birthday: "1990-12-13", gender: "M", dni: 34967836, cuit: 20349678368})
+client2 = Client.create!({first_name: "German", last_name: "Gonzales", birthday: "1994-06-12", gender: "M", dni: 24657867, cuit: 20246578679})
 
-Person.create!([{name: "Person 1", cuit: 30234354679, legal: 1},
-                {name: "Person 2", cuit: 24344324568, legal: 0},
-                {name: "Person 3", cuit: 20233546748, legal: 0}
+person1 = Person.create!({name: "Person 1", cuit: 30234354679, legal: 1})
+person2 = Person.create!({name: "Person 2", cuit: 24344324568, legal: 0})
+person3 = Person.create!({name: "Person 3", cuit: 20233546748, legal: 0})
+puts client1.id
+Bill.create!([{client_id:client1.id, person_id: person2.id, description: "Bill description 1", total_amount: 111, issue_date: "2015-12-13"},
+              {client_id: client1.id, person_id: person3.id, description: "Bill description 2", total_amount: 222, issue_date: "2015-12-13"},
+              {client_id: client2.id, person_id: person1.id, description: "Bill description 3", total_amount: 333, issue_date: "2014-12-13"}
 ])
 
-Bill.create!([{client_id: 1, person_id: 2, description: "Bill description 1", total_amount: 111, issue_date: "2015-12-13"},
-              {client_id: 1, person_id: 3, description: "Bill description 2", total_amount: 222, issue_date: "2015-12-13"},
-              {client_id: 2, person_id: 1, description: "Bill description 3", total_amount: 333, issue_date: "2014-12-13"}
-])
-
-ContactInfo.create!([{client_id: 1, name: "email", value: "federicogratti@gmail.com"},
-                     {client_id: 1, name: "email", value: "federicogratti@live.com.ar"},
-                     {client_id: 1, name: "address", value: "13 n 1139"},
-                     {client_id: 1, name: "phone_number", value: 2214634122},
-                     {client_id: 1, name: "cell_number", value: 2215478554},
-                     {client_id: 1, name: "facebook", value: "fede.gratti"},
-                     {client_id: 2, name: "email", value: "germangonzales@gmail.com"},
-                     {client_id: 2, name: "address", value: "3 y 65"},
-                     {client_id: 2, name: "phone_number", value: 1178646748},
-                     {client_id: 2, name: "facebook", value: "german.gonzales"},
-                     {client_id: 2, name: "skype", value: "german.gonzales"},
+ContactInfo.create!([{client_id: client1.id, name: "email", value: "federicogratti@gmail.com"},
+                     {client_id: client1.id, name: "email", value: "federicogratti@live.com.ar"},
+                     {client_id: client1.id, name: "address", value: "13 n 1139"},
+                     {client_id: client1.id, name: "phone_number", value: 2214634122},
+                     {client_id: client1.id, name: "cell_number", value: 2215478554},
+                     {client_id: client1.id, name: "facebook", value: "fede.gratti"},
+                     {client_id: client2.id, name: "email", value: "germangonzales@gmail.com"},
+                     {client_id: client2.id, name: "address", value: "3 y 65"},
+                     {client_id: client2.id, name: "phone_number", value: 1178646748},
+                     {client_id: client2.id, name: "facebook", value: "german.gonzales"},
+                     {client_id: client2.id, name: "skype", value: "german.gonzales"},
 ])
